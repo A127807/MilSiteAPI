3240acec45b4fc9ac8b7830501e38926c0177d9d
a127807 <PETE_FITTANTE@progressive.com>
2021-09-08 16:56:56 -0400

    Create Images Controller

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilSiteAPI.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class ImagesController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Reading all Locations Images ");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok($"Reading a Locations Images #{id}.");
+		}
+
+		[HttpPost]
+		public IActionResult Post()
+		{
+			return Ok("Creating a Locations Images");
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id)
+		{
+			return Ok($"Update a Locations Images #{id}.");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			return Ok($"Deleted a Locations Images #{id}.");
+		}
+	}
+}
