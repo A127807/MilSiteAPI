aef9c74f003a9566cf88d9a5acb7e0b5efededcb
a127807 <PETE_FITTANTE@progressive.com>
2021-09-08 16:49:17 -0400

    Create Amenities Controller

---
+++ Controllers/AmenitiesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilSiteAPI.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class AmenitiesController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Reading all Locations Amenities ");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok($"Reading a Locations Amenities #{id}.");
+		}
+
+		[HttpPost]
+		public IActionResult Post()
+		{
+			return Ok("Creating a Locations Amenities");
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id)
+		{
+			return Ok($"Update a Locations Amenities #{id}.");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			return Ok($"Deleted a Locations Amenities #{id}.");
+		}
+	}
+}
