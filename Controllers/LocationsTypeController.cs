08b4ccd711702a752e2ffb5d485e2015cb2f0258
a127807 <PETE_FITTANTE@progressive.com>
2021-09-08 16:16:25 -0400

    Created Locations Type Controller. Changed routing solution.

--- Controllers/LocationsTypeController.cs
+++ Controllers/LocationsTypeController.cs
@@ -4,40 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MilSiteAPI.Controllers 
+namespace MilSiteAPI.Controllers
 {
 	[ApiController]
 	[Route("api/v1/[controller]")]
-	public class LocationsController : ControllerBase
+	public class LocationsTypeController : ControllerBase
 	{
+
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok("Reading all Locations");
+			return Ok("Reading all Location Type Type");
 		}
 
 		[HttpGet("{id}")]
 		public IActionResult GetById(int id)
 		{
-			return Ok($"Reading a Location #{id}.");
+			return Ok($"Reading a Location Type #{id}.");
 		}
 
 		[HttpPost]
 		public IActionResult Post()
 		{
-			return Ok("Creating a location");
+			return Ok("Creating a Location Type");
 		}
 
 		[HttpPut("{id}")]
 		public IActionResult Put(int id)
 		{
-			return Ok($"Update Location #{id}.");
+			return Ok($"Update Location Type #{id}.");
 		}
 
 		[HttpDelete("{id}")]
 		public IActionResult Delete(int id)
 		{
-			return Ok($"Deleted Location #{id}.");
+			return Ok($"Deleted Location Type #{id}.");
 		}
 	}
 }
