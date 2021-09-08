fc458520ce6dd0d5e142a333875377eaa63b65ee
a127807 <PETE_FITTANTE@progressive.com>
2021-09-08 16:43:24 -0400

    Create Address Controller

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilSiteAPI.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class AddressController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Reading all Locations Addresses ");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok($"Reading a Locations Address #{id}.");
+		}
+
+		[HttpPost]
+		public IActionResult Post()
+		{
+			return Ok("Creating a Locations Address");
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id)
+		{
+			return Ok($"Update a Locations Address #{id}.");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			return Ok($"Deleted a Locations Address #{id}.");
+		}
+	}
+}
---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilSiteAPI.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class ContactsController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Reading all Locations Contacts ");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok($"Reading a Locations Contacts #{id}.");
+		}
+
+		[HttpPost]
+		public IActionResult Post()
+		{
+			return Ok("Creating a Locations Contacts");
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id)
+		{
+			return Ok($"Update a Locations Contacts #{id}.");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			return Ok($"Deleted a Locations Contacts #{id}.");
+		}
+	}
+}
