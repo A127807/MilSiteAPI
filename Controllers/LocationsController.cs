43c946c99c352689f7347a02a70ddec0deb38db9
a127807 <PETE_FITTANTE@progressive.com>
2021-09-08 15:52:13 -0400

    Created basic routes and return statements for Locations comtroller.

--- Controllers/LocationsController.cs
+++ Controllers/LocationsController.cs
@@ -9,5 +9,39 @@ namespace MilSiteAPI.Controllers
 	[ApiController]
 	public class LocationsController: ControllerBase
 	{
+		[HttpGet]
+		[Route("api/v1/locations")]
+		public IActionResult Get()
+		{
+			return Ok("Reading all Locations");
+		}
+
+		[HttpGet]
+		[Route("api/v1/locations/{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok($"Reading a Location #{id}.");
+		}
+
+		[HttpPost]
+		[Route("api/v1/locations")]
+		public IActionResult Post()
+		{
+			return Ok("Creating a location");
+		}
+
+		[HttpPut]
+		[Route("api/v1/locations/{id}")]
+		public IActionResult Put(int id)
+		{
+			return Ok($"Update Location #{id}.");
+		}
+
+		[HttpDelete]
+		[Route("api/v1/locations/{id}")]
+		public IActionResult Delete(int id)
+		{
+			return Ok($"Deleted Location #{id}.");
+		}
 	}
 }
