dd009c9a3bbbc2a99a589863cedd6c1f1bd5324c
a127807 <PETE_FITTANTE@progressive.com>
2021-09-08 16:28:20 -0400

    Created Features Controller

--- Controllers/FeaturesController.cs
+++ Controllers/FeaturesController.cs
@@ -8,37 +8,38 @@ namespace MilSiteAPI.Controllers
 {
 	[ApiController]
 	[Route("api/v1/[controller]")]
-	public class LocationsTypeController : ControllerBase
+	public class FeaturesController : ControllerBase
 	{
 
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok("Reading all Location Types");
+			return Ok("Reading all Locations Features ");
 		}
 
 		[HttpGet("{id}")]
 		public IActionResult GetById(int id)
 		{
-			return Ok($"Reading a Location Type #{id}.");
+			return Ok($"Reading a Locations Feature #{id}.");
 		}
 
 		[HttpPost]
 		public IActionResult Post()
 		{
-			return Ok("Creating a Location Type");
+			return Ok("Creating a Locations Features");
 		}
 
 		[HttpPut("{id}")]
 		public IActionResult Put(int id)
 		{
-			return Ok($"Update a Location Type #{id}.");
+			return Ok($"Update a Locations Features #{id}.");
 		}
 
 		[HttpDelete("{id}")]
 		public IActionResult Delete(int id)
 		{
-			return Ok($"Deleted Location Type #{id}.");
+			return Ok($"Deleted a Locations Features #{id}.");
 		}
 	}
 }
+
