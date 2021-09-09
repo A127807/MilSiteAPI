f731b7ec02e6ba910ed622bcf4406ca44102a338
a127807 <pete_fittante@progressive.com>
2021-09-09 15:41:41 -0400

    Set up serives to use In memory database via Entity framework

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MilSiteAPI.Filters;
+using MilSiteDataStore.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,15 @@ namespace MilSiteAPI
 {
 	public class Startup
 	{
+		private readonly IWebHostEnvironment _env;
+
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
+		}
+		public Startup(IWebHostEnvironment env)
+		{
+			this._env = env;
 		}
 
 		public IConfiguration Configuration { get; }
@@ -23,6 +31,13 @@ namespace MilSiteAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			if (_env.IsDevelopment())
+			{
+				services.AddDbContext<SiteContext>(options =>
+				{
+					options.UseInMemoryDatabase("Site");
+				});
+			}
 			services.AddControllers();
 
 			//The following statement adds a filter to all controllers
@@ -33,11 +48,15 @@ namespace MilSiteAPI
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SiteContext context)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
+
+				//Create in-memory database for dev environment
+				context.Database.EnsureDeleted();
+				context.Database.EnsureCreated();
 			}
 			else
 			{
