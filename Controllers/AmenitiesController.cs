e0ccc6e3036f03d32a2822116854f2841efad2bc
a127807 <pete_fittante@progressive.com>
2021-09-10 18:56:59 -0400

    Updated endpoints on Amenitites controller

--- Controllers/AmenitiesController.cs
+++ Controllers/AmenitiesController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MilSiteCore.Models;
+using MilSiteDataStore.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,34 +14,79 @@ namespace MilSiteAPI.Controllers
 	public class AmenitiesController : ControllerBase
 	{
 
+		private readonly SiteContext _db;
+
+		public AmenitiesController(SiteContext db)
+		{
+			this._db = db;
+		}
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok("Reading all Locations Amenities ");
+
+			return Ok(_db.Amenities.ToList());
 		}
 
 		[HttpGet("{id}")]
 		public IActionResult GetById(int id)
 		{
-			return Ok($"Reading a Locations Amenities #{id}.");
+			var amenities = _db.Amenities.Find(id);
+			if (amenities == null)
+				return NotFound();
+
+			return Ok(amenities);
 		}
 
 		[HttpPost]
-		public IActionResult Post()
+		public IActionResult Post([FromBody] Amenitie amentities)
 		{
-			return Ok("Creating a Locations Amenities");
+			_db.Amenities.Add(amentities);
+			_db.SaveChanges();
+
+			return CreatedAtAction(nameof(GetById),
+				new { id = amentities.AmID },
+				amentities
+				);
+		}
+
+		[HttpPost]
+		[Route("/api/v2/amenities")]
+		public IActionResult PostV2([FromBody] Amenitie amentities)
+		{
+			_db.Amenities.Add(amentities);
+			_db.SaveChanges();
+
+			return Ok(amentities);
 		}
 
 		[HttpPut("{id}")]
-		public IActionResult Put(int id)
+		public IActionResult Put(int id, Amenitie amentities)
 		{
-			return Ok($"Update a Locations Amenities #{id}.");
+			if (id != amentities.AmID) return BadRequest();
+			_db.Entry(amentities).State = EntityState.Modified;
+			try
+			{
+				_db.SaveChanges();
+			}
+			catch
+			{
+				if (_db.Amenities.Find(id) == null)
+					return NotFound();
+			}
+			return NoContent();
 		}
 
 		[HttpDelete("{id}")]
 		public IActionResult Delete(int id)
 		{
-			return Ok($"Deleted a Locations Amenities #{id}.");
+			var amenities = _db.Amenities.Find(id);
+			if (amenities == null)
+				return NotFound();
+
+			_db.Remove(amenities);
+			_db.SaveChanges();
+
+			return Ok(amenities);
 		}
 	}
 }
