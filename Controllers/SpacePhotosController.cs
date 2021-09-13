25435575c415c4ba9f27136b744de97904c33514
a127807 <pete_fittante@progressive.com>
2021-09-13 07:30:43 -0400

    Added Space Photos and Space controller

---
+++ Controllers/SpacePhotosController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MilSiteCore.Models;
+using MilSiteDataStore.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilSiteAPI.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+
+	//The following statement applies this filter to all the action statements in this controller
+	//[Version1DiscontinueResourceFilter]
+	public class SpacePhotosController : ControllerBase
+	{
+		private readonly SiteContext _db;
+
+		public SpacePhotosController(SiteContext db)
+		{
+			this._db = db;
+		}
+		[HttpGet]
+		public IActionResult Get()
+		{
+
+			return Ok(_db.SpacePhotos.ToList());
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			var spacePhotos = _db.SpacePhotos.Find(id);
+			if (spacePhotos == null)
+				return NotFound();
+
+			return Ok(spacePhotos);
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] SpacePhoto spacePhoto)
+		{
+			_db.SpacePhotos.Add(spacePhoto);
+			_db.SaveChanges();
+
+			return CreatedAtAction(nameof(GetById),
+				new { id = spacePhoto.SpacePhotoId },
+				spacePhoto
+				);
+		}
+
+		[HttpPost]
+		[Route("/api/v2/locations")]
+		public IActionResult PostV2([FromBody] SpacePhoto spacePhoto)
+		{
+			_db.SpacePhotos.Add(spacePhoto);
+			_db.SaveChanges();
+
+			return Ok(spacePhoto);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, SpacePhoto spacePhoto)
+		{
+			if (id != spacePhoto.SpacePhotoId) return BadRequest();
+			_db.Entry(spacePhoto).State = EntityState.Modified;
+			try
+			{
+				_db.SaveChanges();
+			}
+			catch
+			{
+				if (_db.SpacePhotos.Find(id) == null)
+					return NotFound();
+			}
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var spacePhoto = _db.SpacePhotos.Find(id);
+			if (spacePhoto == null)
+				return NotFound();
+
+			_db.Remove(spacePhoto);
+			_db.SaveChanges();
+
+			return Ok(spacePhoto);
+		}
+	}
+}
---
+++ Controllers/SpacesControllers.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MilSiteAPI.Filters;
+using MilSiteCore.Models;
+using MilSiteDataStore.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilSiteAPI.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+
+	//The following statement applies this filter to all the action statements in this controller
+	//[Version1DiscontinueResourceFilter]
+	public class SpacesController : ControllerBase
+	{
+		private readonly SiteContext _db;
+
+		public SpacesController(SiteContext db)
+		{
+			this._db = db;
+		}
+		[HttpGet]
+		public IActionResult Get()
+		{
+
+			return Ok(_db.Spaces.ToList());
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			var space = _db.Spaces.Find(id);
+			if (space == null)
+				return NotFound();
+
+			return Ok(space);
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] Space space)
+		{
+			_db.Spaces.Add(space);
+			_db.SaveChanges();
+
+			return CreatedAtAction(nameof(GetById),
+				new { id = space.SpaceId },
+				space
+				);
+		}
+
+		[HttpPost]
+		[Route("/api/v2/locations")]
+		public IActionResult PostV2([FromBody] Space space)
+		{
+			_db.Spaces.Add(space);
+			_db.SaveChanges();
+
+			return Ok(space);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, Space space)
+		{
+			if (id != space.SpaceId) return BadRequest();
+			_db.Entry(space).State = EntityState.Modified;
+			try
+			{
+				_db.SaveChanges();
+			}
+			catch
+			{
+				if (_db.Spaces.Find(id) == null)
+					return NotFound();
+			}
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var space = _db.Spaces.Find(id);
+			if (space == null)
+				return NotFound();
+
+			_db.Remove(space);
+			_db.SaveChanges();
+
+			return Ok(space);
+		}
+	}
+}
